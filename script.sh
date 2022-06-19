33839ec95f0c5d01f5e8279fa94105f21a80dd4d
k7rish <100378183+k7rish@users.noreply.github.com>
2022-06-19 16:38:53 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,4 @@
-ssh root@54.88.164.254
-tar -xvzf /root/k8/kishore.tar.gz --directory /root/k8
-kubectl create -f /root/k8/deploy.yaml
+ssh root@54.88.164.254"
+tar -xvzf /home/ubuntu/k8/kishore.tar.gz --directory /home/ubuntu/k8/
+kubectl create -f /home/ubuntu/k8/deploy.yaml
+"
